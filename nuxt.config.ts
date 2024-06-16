cea020661985249b5f1706e788356001fce3a1f8
hybridx <hybridx18@gmail.com>
2024-06-16 23:32:17 +0530

    Auto detect for deployment using github variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/` || '/',
+  },
   routeRules: {
     '/': { prerender: true },
   },
--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script setup lang="ts">
-const mainImagePath = `https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/main/images/main.web`;
+const mainImagePath = `https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/main/images/main.webp`;
 
 // const data = await $fetch('https://api.github.com/repos/hybridx/my-moments/contents/images');
 // const items = data.reduce((acc, el) => {
