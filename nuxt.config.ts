5d28322241a0ce3fc277adcbf1aac072c397d47f
hybridx <hybridx18@gmail.com>
2024-06-17 00:42:19 +0530

    Added baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/` || '/',
+    baseURL: `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/`,
   },
   routeRules: {
     '/': { prerender: true },
