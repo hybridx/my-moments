f9170bc52be291265672f7eaf1f05079eb2809ac
hybridx <hybridx18@gmail.com>
2024-06-17 19:48:21 +0530

    Added multiple folders

--- src/components/TheGallery.vue
+++ src/components/TheGallery.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="gallery">
-    <img v-for="image in images" :src="image.download_url" alt="Couple" loading="lazy" />
+    <!-- <img v-for="image in images" :src="image.download_url" alt="Couple" loading="lazy" /> -->
+  </div>
+  <div class="gallery" :id="props.name">
+    <a
+      :href="image.download_url"
+      target="_blank"
+      data-pswp-width="1669"
+      data-pswp-height="2500"
+      v-for="image in images"
+    >
+      <img :src="image.download_url" alt="Couple" loading="lazy" width="340" />
+    </a>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue';
 import { fetchImageUrls } from './../../service/api.js';
 
+// -----
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+
+setTimeout(() => {
+  const lightbox = new PhotoSwipeLightbox({
+    gallery: `#${props.name}`,
+    children: 'a',
+    initialZoomLevel: 'fit',
+    pswpModule: () => import('photoswipe'),
+  });
+
+  lightbox.init();
+}, 100);
+// ------
+
 const props = defineProps({
   name: String,
 });
-
 const images = ref();
 images.value = await fetchImageUrls(props.name);
 </script>
+
 <style scoped lang="scss">
 .gallery {
   display: flex;
